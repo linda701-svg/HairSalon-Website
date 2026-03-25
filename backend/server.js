de845dbfc9754445ea1899ce344dfe9542297e07
Linda <lindaqueentin701@gmail.com>
2026-03-25 14:31:03 +0530

    update the frontend link

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://hairsalon-booking-6bg7.onrender.com', 
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
